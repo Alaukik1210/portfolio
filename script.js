efca37ed8a805e9a1381d8677d68fcdbf5d2e7e4
Alaukik Miishra <151136125+Alaukik1210@users.noreply.github.com>
2024-07-01 00:24:32 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzW7RC-p6T_fDsoV8MzJ3f33W80SzdWAxcS7K3eYV-i_Wj68bMz7igyFB-Wz_0mqzuH/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg")
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+        msg.innerHTML = "*Message sent succesfully*"
+        setTimeout(function(){
+            msg.innerHTML = ""
+
+        },5000)
+        form.reset()
+    })
+    .catch(error => console.error('Error!', error.message))
+})
